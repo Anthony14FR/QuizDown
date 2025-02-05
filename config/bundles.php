7c9d30f0017173c0da61e8836817d0efe780c74d
Jeyrem75 <ly.jerem@gmail.com>
2025-02-05 11:44:22 +0100

    removed monolog bundle

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
